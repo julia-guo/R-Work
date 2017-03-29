dab8f9b6e248de773cf441cf9d502a166a129399
Julia Guo <julia.z.guo@gmail.com>
2017-03-29 17:52:14 -0400

    work with R

---
+++ fakenews.R
@@ -0,0 +1,13 @@
+#Author: Julia Guo
+#Date:2/11/2017
+#Subj: This script uses Kaggle data on fake news to teach Naive Bayes
+## and demonstrate how to use R.
+?install.packages
+install.packages("plyr")
+library(package = "plyr")
+
+?read.csv
+fake <- read.csv("~/Dropbox/SHP/L1_NaiveBayes/fake_newss/fake.csv")
+?class
+class(x)
+
